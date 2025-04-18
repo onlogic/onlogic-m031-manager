15789e36b26a39f24ab3f685339b8dfe826db186
Nick Hanna <nick.hanna@onlogic.com>
2025-04-17 18:00:39 -0700

    initial inheritance

---
+++ src/AutomotiveHandler.py
@@ -0,0 +1,6 @@
+from OnLogicNuvotonManager import OnLogicNuvotonManager
+
+class AutomotiveHandler(OnLogicNuvotonManager):
+    def __init__(self):
+        super().__init__()
+
---
+++ src/DioHandler.py
@@ -0,0 +1,5 @@
+from OnLogicNuvotonManager import OnLogicNuvotonManager
+
+class DioHandler(OnLogicNuvotonManager):
+    def __init__(self):
+        super().__init__()
